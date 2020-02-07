70260a470c36ec784ee519a5313cc115e26e45bf
HysenChan <17602059496@163.com>
2020-02-07 14:01:27 +0800

    添加一个Unity菜单Tools工具，用于设置游戏参数

---
+++ Assets/Scripts/Manager/GameInit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInit : MonoBehaviour
+{
+    [Space(5)]
+    [Header("Settings")]
+    public string LevelMusic = "Music";
+    public string showMenuAtStart = "";
+    public bool playMusic = true;
+    public bool createUI;
+    public bool createInputManager;
+    public bool createAudioPlayer;
+    public bool createGameCamera;
+    private GameObject audioPlayer;
+    private GameSettings settings;
+
+    
+}
