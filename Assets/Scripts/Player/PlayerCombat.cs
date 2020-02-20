7e2a2600095ca8ea9ef213ba54443ad83d002d81
HysenChan <17602059496@163.com>
2020-02-20 23:50:58 +0800

    【添加】添加一个角色战斗脚本PlayerCombat，初始化相关组件

---
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(PlayerState))]
+public class PlayerCombat : MonoBehaviour
+{
+    [Header("Linked Components")]
+    private PlayerAnimator playerAnimator;//拿到动画制作器组件
+    private PlayerState playerState;//拿到玩家的状态
+    private Rigidbody rb;//拿到刚体
+    private InputManager inputManager;//拿到输入管理器
+
+    private void Start()
+    {
+        playerAnimator = GetComponentInChildren<PlayerAnimator>();
+        playerState = GetComponent<PlayerState>();
+        rb = GetComponent<Rigidbody>();
+        inputManager = FindObjectOfType<InputManager>();
+
+        //找不到组件时输出到控制台
+        if (!playerAnimator)
+            Debug.LogError(gameObject.name+"找不到PlayerAnimator脚本组件！");
+        if (!playerState)
+            Debug.LogError(gameObject.name + "找不到PlayerState脚本组件！");
+        if (!rb)
+            Debug.LogError(gameObject.name + "找不到Rigidbody组件！");
+    }
+}
