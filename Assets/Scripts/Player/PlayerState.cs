e010efe8ea8176ce3ece0cc2a26c6ceb501a662a
HysenChan <17602059496@163.com>
2020-02-22 17:42:03 +0800

    【修改】修改角色状态，添加拳击和踢

--- Assets/Scripts/Player/PlayerState.cs
+++ Assets/Scripts/Player/PlayerState.cs
@@ -14,26 +14,28 @@ public class PlayerState : MonoBehaviour
     {
         currentState = state;
     }
-
-    public enum PLAYERSTATE
-    {
-        IDLE,   //闲置状态
-        WALK,   //行走
-        JUMPING,    //跳
-        LAND,   //
-        JUMPKICK,   //跳起来踢
-        ATTACK, //攻击
-        DEFEND,//防御
-        HIT,//击中
-        DEATH,//死亡
-        THROW,//扔
-        PICKUPITEM,//抬起物体
-        KNOCKDOWN,//击倒
-        KNOCKDOWNGROUNDED,//击倒接地
-        GROUNDPUNCH,//重拳
-        GROUNDKICK,//重腿
-        GROUNDHIT,//重击
-        STANDUP,//站立状态
-        USEWEAPON,//使用武器
-    };
 }
+
+public enum PLAYERSTATE
+{
+    IDLE,   //闲置状态
+    WALK,   //行走
+    JUMPING,    //跳
+    LAND,   //落地
+    JUMPKICK,   //跳踢
+    PUNCH,//拳击
+    KICK,//踢
+    ATTACK, //攻击
+    DEFEND,//防御
+    HIT,//击中
+    DEATH,//死亡
+    THROW,//扔
+    PICKUPITEM,//抬起物体
+    KNOCKDOWN,//击倒
+    KNOCKDOWNGROUNDED,//击倒接地
+    GROUNDPUNCH,//重拳
+    GROUNDKICK,//重腿
+    GROUNDHIT,//重击
+    STANDUP,//站立状态
+    USEWEAPON,//使用武器
+};
